0a2e95b0f58ccd6dba91b670dd2407cc03bc040e
Darius Valancauskas <darius.veracity@gmail.com>
2021-07-29 08:49:19 +0300

    Added PasswordAuthenticatedUserInterface to remove deprecation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,13 +8,14 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\UserRepository;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\HasLifecycleCallbacks
  */
-class User implements UserInterface
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     /**
      * @ORM\Id()
